89c0aff2d9312af4010c6040a147023499a4d018
Schwartz <ejschwartz1@wpi.edu>
2023-07-26 10:05:32 -0400

    added background color toggle

--- intro/intro.js
+++ intro/intro.js
@@ -2,14 +2,19 @@ function clickButton() {
     console.log(`Hello World!`);
     const myText = document.getElementById('myText');
     const body = document.body;
-    const button = document.getElementById(`modeButton`)
+    const button = document.getElementById(`modeButton`);
+    const label = document.getElementById(`label`);
+    const color = document.getElementById(`color`).value;
     if (button.innerHTML == `Dark Mode`) {
         button.innerHTML = `Light Mode`;
         body.style.backgroundColor = `black`;
         myText.style.color = `white`;
+        label.style.color = `white`;
     } else {
         button.innerHTML = `Dark Mode`;
         body.style.backgroundColor = `lavender`;
         myText.style.color = `black`;
+        label.style.color = `black`;
     }
+    if (color != ``) body.style.backgroundColor = color;
 }
