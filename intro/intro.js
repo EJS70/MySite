01efac7e48c311c5657ff58a062e2f8fb445ccb9
Schwartz <ejschwartz1@wpi.edu>
2023-07-26 09:36:15 -0400

    added light/dark mode changer

--- intro/intro.js
+++ intro/intro.js
@@ -1,3 +1,15 @@
 function clickButton() {
-    console.log(`Hello World!`)
+    console.log(`Hello World!`);
+    const myText = document.getElementById('myText');
+    const body = document.body;
+    const button = document.getElementById(`modeButton`)
+    if (button.innerHTML == `Dark Mode`) {
+        button.innerHTML = `Light Mode`;
+        body.style.backgroundColor = `black`;
+        myText.style.color = `white`;
+    } else {
+        button.innerHTML = `Dark Mode`;
+        body.style.backgroundColor = `lavender`;
+        myText.style.color = `black`;
+    }
 }
