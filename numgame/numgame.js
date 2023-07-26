8b86c33db5559ed65c9dc60fb8f843730bd9892f
Schwartz <ejschwartz1@wpi.edu>
2023-07-26 14:56:54 -0400

    added numgame and glowing header

--- numgame/numgame.js
+++ numgame/numgame.js
@@ -1,14 +1,34 @@
 const guess = document.getElementById(`guess`);
 const result = document.getElementById(`result`);
+const button = document.getElementById(`button`);
+const label = document.getElementById(`label`);
+const rickroll = document.getElementById("rickroll");
 
 const MAXIMUM = 100;
 let correct;
+let guessCount;
 function loadGame() {
     guess.max = MAXIMUM;
-    correct = Math.random() * (MAXIMUM + 1);
-    correct = Math.floor(correct);
+    correct = Math.floor(Math.random() * (MAXIMUM + 1));
+    guessCount = 1;
 }
 
 function submitGuess() {
-
+    if (guess.value > 100 || guess.value < 0) {
+        result.innerHTML = `Please enter a number between 0 and 100.`;
+    } else if (guess.value > correct) {
+        result.innerHTML = `That guess is too high!`;
+        guessCount++;
+    } else if (guess.value < correct) {
+        result.innerHTML = `That guess is too low!`;
+        guessCount++;
+    } else {
+        result.innerHTML = `That guess is correct! It took you ` + guessCount +
+                            (guessCount == 1 ? ` guess` :  ` guesses`) + 
+                            ` to find the correct number.`;
+        button.style.display = `none`;
+        guess.style.display = `none`;
+        label.innerHTML = `Answer: ` + correct + `!`;
+        label.className = "animLabel"
+    }
 }
